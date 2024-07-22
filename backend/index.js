7f491fb51edb536645c84f04a6d123b68d3a71e3
Yacine Sinapayen <yacinesinapayen@Air-de-Yacine.lan>
2024-07-22 08:18:47 +0200

    fix cors origin access

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,24 @@ import express from "express";
 import mongoose from "mongoose";
 import { mongoDBURL, PORT } from "./config.js";
 import BooksRoute from "./routes/booksRoute.js";
+import cors from "cors";
 
 const app = express();
 
 // Middleware for parsing request body
 app.use(express.json());
+
+// Middleware for handling CORS
+// option 1 Allow all origins with default of cors(*)
+app.use(cors());
+// Option 2 Allow only specific origins
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET,POST,PUT,DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 
 app.get("/", (req, response) => {
   console.log(req);
